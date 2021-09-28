97ebad86b8c9097d475cc87e4eed848b7206c4a1
leorambo <leorambo@163.com>
2021-09-28 11:04:57 +0800

    添加文件

---
+++ Hello/src/main/java/com/java/maven/HelloTest.java
@@ -0,0 +1,15 @@
+package com.java.maven;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author LeoRambo
+ * @create 2021-09-28-10:18
+ */
+public class HelloTest {
+
+    @Test
+    public void test(){
+        System.out.println("Hello , maven!");
+    }
+}
